ffbcc0ec3787a8597ac9f331d3a2feb70aff6932
Ricky Kuang <rkuang26@gmail.com>
2018-05-17 19:53:33 -0700

    find last non overlapping

--- payoff_sched.cpp
+++ payoff_sched.cpp
@@ -5,13 +5,25 @@
 using namespace std;
 
 int main() {
-    vector<Activity*> activities;
+    vector<Activity *> activities;
     int start, end, payoff;
     while (cin >> start >> end >> payoff) {
         activities.push_back(new Activity(start, end, payoff));
     }
 
     sort(activities.begin(), activities.end(), ActivityComparator());
+
+    int n = activities.size();
+    int opt[n+1];
+    opt[0] = 0;
+
+    int q;
+    for (int i=0; i<n; i++) {
+        q = lastNonOverlapping(activities, i);
+        opt[i+1] = max(opt[i], activities[i]->payoff+opt[q]);
+    }
+
+    cout << "Maximum Payoff:\t" << opt[n] << endl;
 
     return 0;
 }
--- payoff_sched.hpp
+++ payoff_sched.hpp
@@ -1,6 +1,10 @@
 #ifndef PAYOFF_SCHED_H
 #define PAYOFF_SCHED_H
 
+#include <algorithm>
+
+using namespace std;
+
 class Activity {
     public:
         int start;
@@ -20,5 +24,29 @@ struct ActivityComparator {
         return a1->end < a2->end;
     };
 };
+
+int lastNonOverlapping(const vector<Activity *> activities, int i) {
+    // for (int j = i-1; j >= 0; j--) {
+    //     if (activities[i]->start > activities[j]->end)
+    //         return j+1;
+    // }
+
+    int val = 0;
+
+    int left = 0;
+    int right = i;
+    int mid;
+    while (left <= right) {
+        mid = left + (right-left)/2;
+        if (activities[mid]->end > activities[i]->start)
+            right = mid - 1;
+        else {
+            val = mid;
+            left = mid + 1;
+        }
+    }
+
+    return val;
+}
 
 #endif
