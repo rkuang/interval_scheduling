3e93e8fb465f0b3a1721f71c4cb642c4b72abcbd
Ricky Kuang <rkuang26@gmail.com>
2018-05-24 18:50:23 -0700

    save and output activities

--- payoff_sched.cpp
+++ payoff_sched.cpp
@@ -17,13 +17,27 @@ int main() {
     int opt[n+1];
     opt[0] = 0;
 
+    vector<Activity *> out[n+1];
+
     int q;
     for (int i=0; i<n; i++) {
         q = lastNonOverlapping(activities, i);
-        opt[i+1] = max(opt[i], activities[i]->payoff+opt[q]);
+        // opt[i+1] = max(opt[i], activities[i]->payoff+opt[q]);
+        if (opt[i] >= activities[i]->payoff+opt[q]) {
+            opt[i+1] = opt[i];
+            out[i+1] = out[i];
+        } else {
+            opt[i+1] = activities[i]->payoff+opt[q];
+            out[i+1] = out[q];
+            out[i+1].push_back(activities[i]);
+        }
     }
 
     cout << "Maximum Payoff: " << opt[n] << endl;
+    vector<Activity *>::iterator it;
+    for (it=out[n].begin(); it!=out[n].end(); it++) {
+        cout << (*it)->start << " " << (*it)->end << " " << (*it)->payoff << endl;
+    }
 
     return 0;
 }
